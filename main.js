3d0bf1161a1c8644058162347510b83c817adfb7
Leonardo Puebla <leonardo38361@gmail.com>
2022-03-28 16:43:18 -0300

    Se crean nuevas propiedades con defineProperty

--- main.js
+++ main.js
@@ -14,11 +14,29 @@ const juan = {
 // console.log( Object.getOwnPropertyNames(juan));
 // console.log( Object.entries(juan));
 
-console.log(Object.getOwnPropertyDescriptors(juan));
-
-Object.defineProperty(juan, "pruebaNasa", {
-    value: "extraterrestres",
+Object.defineProperty(juan, "navigator", {
+    value: "Chrome",
     writable: true,
     configurable: true,
+    enumerable: false
+});
+Object.defineProperty(juan, "editor", {
+    value: "VSCode",
+    configurable: true,
+    writable: false,
     enumerable: true
+});
+Object.defineProperty(juan, "pruebaNasa", {
+    value: "extraterrestres",
+    enumerable: false,
+    writable: false,
+    configurable: false,
 })
+Object.defineProperty(juan, "terminal", {
+    value: "WSL",
+    enumerable: true,
+    writable: true,
+    configurable: false,
+})
+
+console.log(Object.getOwnPropertyDescriptors(juan));
