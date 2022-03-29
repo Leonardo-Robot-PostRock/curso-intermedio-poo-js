a5aec41b44bfb7f3eef4ecff9831b26754385617
Leonardo Puebla <leonardo38361@gmail.com>
2022-03-28 23:19:28 -0300

    recursividad

--- main.js
+++ main.js
@@ -13,3 +13,28 @@ obj1 = {
 const stringifiedComplexObj = JSON.stringify(obj1); // stringify;
 const obj2 = JSON.parse(stringifiedComplexObj); // parse;
 
+// function recursiva(){
+//     if(/*validación*/){
+//        // llamados recursivos
+//     }else{
+
+//     }
+// }
+const numeritos = [0, 1, 2, 3, 4, 5, 5, 5, 5, 6, 7, 8, 8, 8, 9, 9];
+// let numerito = 0;
+// for (let index = 0; index < numeritos.length; index++) {
+//     numerito = numeritos[index];
+//     console.log({ index, numerito });
+// }
+
+function recursiva(numbersArray) {
+    if (numbersArray.length !== 0) {
+        const firstNum = numbersArray[0];
+        console.log(firstNum);
+        
+        numbersArray.shift();
+        recursiva(numbersArray);
+    } else {
+
+    }
+}
